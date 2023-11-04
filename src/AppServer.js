26dca02e8686dc260040240a3b06bffc991641c4
Masoud Mohammadi <masoudmhmdi86@gmail.com>
2023-11-04 19:17:33 +0330

    feat:RSC
    create first react-server-component

---
+++ src/AppServer.js
@@ -0,0 +1,12 @@
+// src/AppServer.js
+import React from 'react';
+
+const AppServer = () => {
+  return (
+    <div>
+      <h1>Hello from Server-Side Rendered React App!</h1>
+    </div>
+  );
+};
+
+export default AppServer;
